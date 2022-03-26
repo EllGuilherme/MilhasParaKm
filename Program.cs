dd2c9a66269b5c16647274a49d9bf7661b2664a4
EllGuilherme <grds1000@gmail.com>
2022-03-26 16:50:03 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿double  milhas, kms;
+
+Console.Write("Entre com a medida (em milhas): "); 
+milhas = Convert.ToDouble(Console.ReadLine());
+
+
+kms = (milhas * 1.609);
+
+Console.WriteLine($"{kms:N3} km");
+
